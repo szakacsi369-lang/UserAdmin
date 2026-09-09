be5fa56cf4e6945373c0383ee9f7b76b444ea185
szakacsi369 <szakacsi369@gmail.com>
2026-09-09 18:58:20 +0200

    userdbservice

---
+++ UserAdmin/Services/UserDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAdmin.Services
+{
+    internal class UserDbService
+    {
+        public string ConnectionString = "Server=localhost;Database=useradmin;User=root;Password=;";
+    }
+}
