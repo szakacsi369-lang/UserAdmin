3a8c342c5d6bf9027f9579eb0af4ff3c0003442c
szakacsi369 <szakacsi369@gmail.com>
2026-09-10 17:38:21 +0200

    ready

--- UserAdmin/Services/UserDbService.cs
+++ UserAdmin/Services/UserDbService.cs
@@ -15,7 +15,16 @@ namespace UserAdmin.Services
             var connection = new MySqlConnection(ConnectionString);
             connection.Open();
 
-            string sql = "";
+            string sql = @"INSERT INTO `users`(`username`, `Email`, `password`, `registeredAt`) 
+            VALUES (@Username,@Email,@Password,@RegisteredAt)";
+
+            var cmd = new MySqlCommand(sql, connection);
+
+            cmd.Parameters.AddWithValue("@Username",user.Username);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@RegisteredAt", user.RegisteredAt);
+            cmd.ExecuteNonQuery();
 
             connection.Close();
         }
