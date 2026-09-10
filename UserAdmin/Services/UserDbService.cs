7a7d702a245ed6ac2e2091ea45e903d46bc475a0
szakacsi369 <szakacsi369@gmail.com>
2026-09-10 17:31:33 +0200

    sql statement

--- UserAdmin/Services/UserDbService.cs
+++ UserAdmin/Services/UserDbService.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UserAdmin.Models;
 
 namespace UserAdmin.Services
 {
     internal class UserDbService
     {
         public string ConnectionString = "Server=localhost;Database=useradmin;User=root;Password=;";
+
+        public void Add(User user)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = "";
+
+            connection.Close();
+        }
     }
 }
